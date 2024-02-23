b53464315f8b0d8b922c5e22d9b41c25427d800b
Gustavo0121 <146298926+Gustavo0121@users.noreply.github.com>
2024-02-23 23:08:46 +0000

    refactor: arquivo de base atualizado

--- praticar/base.py
+++ praticar/base.py
@@ -7,30 +7,35 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    """"""
-    page.title = 'Help'
+    """Run this app."""
+    page.title = 'Planalto Legis'
 
     page.appbar = ft.AppBar(
         title=ft.Text(
-        "Ajuda", weight=ft.FontWeight.BOLD, color="white"
+        'Ajuda', weight=ft.FontWeight.BOLD, color='white'
         ),
-        bgcolor="#3b65ad",
+        bgcolor='#3b65ad',
         center_title=True,
-        actions=[ft.IconButton(ft.icons.MENU, tooltip="Menu", icon_color="black")]
+        actions=[ft.IconButton(
+            ft.icons.MENU,
+            tooltip='Menu',
+            icon_color='black',
+            ),
+            ]
     )
 
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationDestination(
-                icon=ft.icons.CALENDAR_MONTH_ROUNDED, label='RESENHA'
+                icon=ft.icons.CALENDAR_MONTH_ROUNDED, label='RESENHA',
             ),
             ft.NavigationDestination(icon=ft.icons.LOUPE, label='BUSCA'),
             ft.NavigationDestination(
-                icon=ft.icons.STAR_PURPLE500_OUTLINED, label='FAVORITOS'
+                icon=ft.icons.STAR_PURPLE500_OUTLINED, label='FAVORITOS',
             ),
             ft.NavigationDestination(icon=ft.icons.HELP, label='AJUDA'),
         ],
-        bgcolor="#3b65ad",
+        bgcolor='#3b65ad',
     )
 
     page.add(ft.Text('Corpo de Aplicacao'))
